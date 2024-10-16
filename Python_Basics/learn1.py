4ffb350d16c5d26dfd0f1cc0aa4d9d3df94de6a0
Rajin-Saleh <rajinp752@gmail.com>
2024-10-16 20:47:45 +0600

    first programm

---
+++ Python_Basics/learn1.py
@@ -0,0 +1,14 @@
+#  This code represent my undersatnding in modules, pip, and comment
+
+#  Single line comment
+''' Multiple line comment '''
+print("Hello Python Coders")
+
+import pyjokes
+
+joke1 = pyjokes.get_joke()
+print(joke1)
+joke2 = pyjokes.get_joke()
+print(joke2)
+
+
