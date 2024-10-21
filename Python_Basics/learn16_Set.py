62d0c368c1513f0e1092f1d92aa12ff516f983a8
Rajin-Saleh <rajinp752@gmail.com>
2024-10-21 21:41:29 +0600

    Sets in Python

---
+++ Python_Basics/learn16_Set.py
@@ -0,0 +1,12 @@
+s = {1, 5, 32, 54, 5, 5, 5}
+
+
+"""'
+whenever we are in a use case where elements can not be repeated than we use set()
+don't use s = {} as it will create an empty dictionary
+when maintaining order is must than use list()
+"""
+
+e = set()
+print(s)
+print(type(e))
