d61dc835a95dd775361a66c61119ac0950dc5fc5
Rajin-Saleh <rajinp752@gmail.com>
2024-10-21 11:12:27 +0600

    Lists

---
+++ Python_Basics/learn13_Lists.py
@@ -0,0 +1,16 @@
+# List are container to store a set of value of any data type
+
+
+friends = ["Apple", "orange", 5, 345.06, False, "Akash", "Rohan"]
+
+# print(friends[0])
+
+
+friends[0] = "Grapes"  # Unlike strings lists are mutable
+
+print(friends[0])
+
+print(friends[1:4])  # list slicing
+
+
+# list can be index just like string.
