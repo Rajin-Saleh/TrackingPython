aa4170ef51d9ec32a4472f02a5198ca374133e27
Rajin-Saleh <rajinp752@gmail.com>
2024-10-21 21:40:13 +0600

    Dictionaries

---
+++ Python_Basics/learn15_Dictionaries.py
@@ -0,0 +1,12 @@
+# Dictionaries in Python
+# List of key-value pairs
+d = {}  # Empty dictionary
+
+marks = {
+    "Harry": 100,
+    "Shiyon": 78,
+    "Roman": 89,
+}
+
+# print(marks, type(marks))
+print(marks["Roman"])
