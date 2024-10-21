cba93e0973dc44309f05e619afc5fb590637c59e
Rajin-Saleh <rajinp752@gmail.com>
2024-10-21 10:47:47 +0600

    List Methods

---
+++ Python_Basics/list_Methods.py
@@ -0,0 +1,23 @@
+friends = ["Apple", "orange", 5, 345.06, False, "Akash", "Rohan"]
+
+# print(friends)
+
+friends.append("Harmony")
+print(friends)
+
+l1 = [1567, 25436456876, 3, 754, 5, 6756, 7, 2345, 345]
+# l1.reverse()
+# print(l1)
+
+l1.insert(2, 456)
+print(l1)
+
+# l1.sort()
+# print(l1)
+value = l1.pop(3)
+print(value)
+print(l1)
+
+remove = l1.remove(456)
+print(remove)
+print(l1)
