374a576e60c352cdc35231e31fbb5ef3039eeb22
Rajin-Saleh <rajinp752@gmail.com>
2024-10-21 11:45:57 +0600

    Tuples

---
+++ Python_Basics/learn14_Touples.py
@@ -0,0 +1,15 @@
+# Tuples in python
+
+
+a = (1, 345, 2345, 3453, 567, False, "Siyam", "Rohan")
+
+print(a[0])
+print(type(a))
+
+
+no = a.count(345)
+print(no)
+
+i = a.index(345)
+print(i)
+print((a[1:3]))
